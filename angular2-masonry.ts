c9de6a1bdc2d5e845c4cc54df70eddfe28a4065d
Jonas Elgblad <jonas.elgblad@gmail.com>
2016-04-08 13:41:28 +0200

    Some minor changes

--- angular2-masonry.ts
+++ angular2-masonry.ts
@@ -6,25 +6,25 @@ import { Component, Input, ElementRef, AfterViewInit, AfterContentChecked } from
 })
 export class AngularMasonry implements AfterViewInit, AfterContentChecked {
 
-    private elem = null;
-    private msnry = null;
-    private itemCount: number;
+    private _elem = null;
+    private _msnry = null;
+    private _itemCount: number;
 
     // Component inputs
     @Input('item-selector') itemSelector: string = '.brick';
     @Input('origin-left') originLeft: boolean = true;
     @Input('origin-top') originTop: boolean = true;
-    @Input() reload: boolean = true;
+    @Input('reload') autoReload: boolean = true;
 
     constructor(
-        private _myElement: ElementRef
+        private _componentElement: ElementRef
     ) { }
 
     private ngAfterViewInit() {
 
-        this.elem = this._myElement.nativeElement.children[0];
+        this._elem = this._componentElement.nativeElement.children[0];
 
-        this.msnry = new Masonry(this.elem, {
+        this._msnry = new Masonry(this._elem, {
             itemSelector: this.itemSelector,
             originLeft: this.originLeft,
             originTop: this.originTop
@@ -33,19 +33,19 @@ export class AngularMasonry implements AfterViewInit, AfterContentChecked {
     
     private ngAfterContentChecked() {
         // Is auto reload enabled?
-        if (this.reload === true) {
+        if (this.autoReload === true) {
 
             // Is component initialized?
-            if (this.elem && this.msnry) {
+            if (this._elem && this._msnry) {
 
                 // Get number of childen in DOM element
-                let count = this.elem.children.length;
+                let count = this._elem.children.length;
 
                 // Has items changed?
-                if (count !== this.itemCount) {
+                if (count !== this._itemCount) {
 
                     // Get itemCount
-                    this.itemCount = count;
+                    this._itemCount = count;
 
                     // Reload items
                     this.reloadItems();
@@ -56,9 +56,10 @@ export class AngularMasonry implements AfterViewInit, AfterContentChecked {
     
     // Reload layout
     public reloadItems() {
+        
         // Reload and layout Masonry
-        this.msnry.reloadItems();
-        this.msnry.layout();
+        this._msnry.reloadItems();
+        this._msnry.layout();
 
         console.log('Masonry: reloadItems');
     }
