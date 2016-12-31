f0b34202adb9f9051a5ca80784868bcef6820d6b
Haris Basic <harisb2012@gmail.com>
2016-12-31 12:52:08 +0100

    Added observer when child tree of elements changes - we can triger layout() at that point.

--- src/brick.ts
+++ src/brick.ts
@@ -1,3 +1,10 @@
+interface MutationWindow extends Window {
+    MutationObserver: any;
+    WebKitMutationObserver: any;
+}
+
+declare var window: MutationWindow;
+
 import {
     Directive,
     Inject,
@@ -21,9 +28,30 @@ export class AngularMasonryBrick implements OnDestroy, AfterViewInit {
 
     ngAfterViewInit() {
         this._parent.add(this._element.nativeElement);
+        this.watchForHtmlChanges();
     }
 
     ngOnDestroy() {
         this._parent.remove(this._element.nativeElement);
+    }
+
+    /** When HTML in brick changes dinamically, observe that and change layout */
+    private watchForHtmlChanges(): void {
+        MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+        if (MutationObserver) {
+            /** Watch for any changes to subtree */
+            let self = this;
+            let observer = new MutationObserver(function(mutations, observerFromElement) {
+                self._parent.layout();
+            });
+
+            // define what element should be observed by the observer
+            // and what types of mutations trigger the callback
+            observer.observe(this._element.nativeElement, {
+                subtree: true,
+                childList: true
+            });
+        }
     }
 }
